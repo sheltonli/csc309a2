d702cfca5ab989412ff62ac5ac3f50107d283ab5
sheltonli <shelton_li@hotmail.com>
2014-03-16 23:33:45 -0400

    login works

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -6,11 +6,26 @@ class User_model extends CI_Model {
     	'first'=>$this->input->post('first'),
     	'last'=>$this->input->post('last'),
    	 	'login'=>$this->input->post('username'),
-   	 	'password'=>md5($this->input->post('password')),
+   	 	'password'=>$this->input->post('password'),
    	 	'email'=>$this->input->post('email')
   		);
 
 		return $this->db->insert('customer',$data);
+	}
+
+	function login(){
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+
+		$this->db->where('login', $username);
+        $this->db->where('password', $password);
+        
+        $query = $this->db->get('customer');
+
+        if (isset($query) && $query->num_rows > 0){
+        	return true;
+        }
+       	return false;
 	}
 }
 ?>
