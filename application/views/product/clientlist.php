eedcf0665a8402786f11a605ff65df810df470e0
sheltonli <shelton_li@hotmail.com>
2014-03-19 13:01:13 -0400

    text change

--- application/views/product/clientlist.php
+++ application/views/product/clientlist.php
@@ -13,8 +13,8 @@
 			echo "<td>" . $product->price . "</td>";
 			echo "<td><img src='" . base_url() . "images/product/" . $product->photo_url . "' width='100px' /></td>";
 				
-			echo "<td>" . anchor("client/deletecandy/$product->id",'Remove Candy from Cart',"onClick='return confirm(\"Do you really want to remove this item?\");'") . "</td>";
-			echo "<td>" . anchor("client/add/$product->id",'Add Candy to Cart') . "</td>";
+			echo "<td>" . anchor("client/deletecandy/$product->id",'Remove from Cart',"onClick='return confirm(\"Do you really want to remove this item?\");'") . "</td>";
+			echo "<td>" . anchor("client/add/$product->id",'Add to Cart') . "</td>";
 			echo "<td>" . anchor("client/read/$product->id",'View Details') . "</td>";
 			if ($this->session->userdata($product->id)) {
 				echo"<td>Added to Cart</td>";
