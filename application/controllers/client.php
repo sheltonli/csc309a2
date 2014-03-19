3be1330deb772604fbcbc58dffbd9c4803af6339
sheltonli <shelton_li@hotmail.com>
2014-03-18 20:53:18 -0400

    forgot client

--- application/controllers/client.php
+++ application/controllers/client.php
@@ -6,17 +6,6 @@ class Client extends CI_Controller {
 	function __construct() {
 		// Call the Controller constructor
 		parent::__construct();
-
-
-		$config['upload_path'] = './images/product/';
-		$config['allowed_types'] = 'gif|jpg|png';
-/*	    	$config['max_size'] = '100';
-		$config['max_width'] = '1024';
-		$config['max_height'] = '768';
- */
-
-		$this->load->library('upload', $config);
-
 	}
 
 	function index() {
